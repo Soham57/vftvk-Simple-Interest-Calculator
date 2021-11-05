9cb5d69c212df44cb967e8f25a544d5c1c4a906f
Soham Malviya <sohammalviya65@gmail.com>
2021-11-05 14:26:17 +0530

    HTML CSS JS Files updated

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  let principal = Number(document.getElementById("principal").value);
+  let rate = Number(document.getElementById("rate").value);
+  let noy = Number(document.getElementById("years").value);
+  let result = document.getElementById("result");
+
+  result.innerHTML = `If you deposit <mark>${principal}</mark>,<br>
+  at an interest rate of <mark>${rate}</mark>.<br>
+  You will recieve an amount of <mark>${principal * rate * noy}</mark>,<br>
+  in the year <mark>${new Date().getFullYear() + noy}</mark>.`;
 }
-        
